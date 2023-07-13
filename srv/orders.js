8e837728aefadc686eb19b60581aed99bfea562a
jegiraldog <jhony.giraldo@globant.com>
2023-07-13 12:12:29 +0000

    Julio 13

--- srv/orders.js
+++ srv/orders.js
@@ -2,8 +2,50 @@ const cds = require("@sap/cds");
 const { Orders } = cds.entities("com.training");
 
 module.exports = (srv) => {
-    srv.on("READ", "GetOrders", async(req) => {
-        console.log(`Method: ${req.method}`);
+
+    srv.on("READ", "Orders", async (req) => {
+        if (req.data.ClientEmail !== undefined) {
+            return await SELECT.from`com.training.Orders`
+                .where`ClientEmail = ${req.data.ClientEmail}`;
+        }
         return await SELECT.from(Orders);
     });
+
+    srv.after("READ", "Orders", (data) => {
+        return data.map((order) => (order.Reviewed = true));
+    });
+
+    //************CREATE******/
+    
+    srv.on("CREATE", "Orders", async (req) => {
+        let returnData = await cds
+            .transaction(req)
+            .run(
+                INSERT.into(Orders).entries({
+                    ClientEmail: req.data.ClientEmail,
+                    FirstName: req.data.FirstName,
+                    LastName: req.data.LastName,
+                    CreatedOn: req.data.CreatedOn,
+                    Reviewed: req.data.Reviewed,
+                    Approved: req.data.Approved,
+                })
+            )
+            .then((resolve, reject) => {
+                console.log("Resolve", resolve);
+                console.log("Reject", reject);
+
+                if (typeof resolve !== "undefined") {
+                    return req.data;
+                } else {
+                    req.error(409, "Record Not Inserted");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                req.error(err.code, err.message);
+            });
+        console.log("Before End", returnData);
+        return returnData;
+    });
+    
 };
