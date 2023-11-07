15744b5c6f49c2d4eda200588a2969b03be48939
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2023-11-07 22:42:30 +0000

    7.8. On DELETE

--- srv/orders.cds
+++ srv/orders.cds
@@ -1,7 +1,5 @@
 using com.training as training from '../db/training';
 
 service ManageOrders {
-    entity GetOrders    as projection on training.Orders;
-    entity CreateOrder  as projection on training.Orders;
-    entity UpdateOrder  as projection on training.Orders;
+    entity Orders    as projection on training.Orders;
 }
--- srv/orders.js
+++ srv/orders.js
@@ -3,58 +3,58 @@ const { Orders } = cds.entities("com.training");
 
 module.exports = (srv) => {
 
-    srv.on("READ", "GetOrders", async (req) => {
-        if (req.data.ClientEmail !== undefined) {
-            return await SELECT.from`com.training.Orders`
-                .where`ClientEmail = ${req.data.ClientEmail}`;
+  srv.on("READ", "Orders", async (req) => {
+    if (req.data.ClientEmail !== undefined) {
+      return await SELECT.from`com.training.Orders`
+        .where`ClientEmail = ${req.data.ClientEmail}`;
+    }
+    return await SELECT.from(Orders);
+  });
+
+  srv.after("READ", "Orders", (data) => {
+    return data.map((order) => (order.Reviewed = true));
+  });
+
+  //************CREATE******/
+
+  srv.on("CREATE", "Orders", async (req) => {
+    let returnData = await cds
+      .transaction(req)
+      .run(
+        INSERT.into(Orders).entries({
+          ClientEmail: req.data.ClientEmail,
+          FirstName: req.data.FirstName,
+          LastName: req.data.LastName,
+          CreatedOn: req.data.CreatedOn,
+          Reviewed: req.data.Reviewed,
+          Approved: req.data.Approved,
+        })
+      )
+      .then((resolve, reject) => {
+        console.log("Resolve", resolve);
+        console.log("Reject", reject);
+
+        if (typeof resolve !== "undefined") {
+          return req.data;
+        } else {
+          req.error(409, "Record Not Inserted");
         }
-        return await SELECT.from(Orders);
-    });
-
-    srv.after("READ", "GetOrders", (data) => {
-        return data.map((order) => (order.Reviewed = true));
-    });
-
-    //************CREATE******/
-    
-    srv.on("CREATE", "CreateOrder", async (req) => {
-        let returnData = await cds
-            .transaction(req)
-            .run(
-                INSERT.into(Orders).entries({
-                    ClientEmail: req.data.ClientEmail,
-                    FirstName: req.data.FirstName,
-                    LastName: req.data.LastName,
-                    CreatedOn: req.data.CreatedOn,
-                    Reviewed: req.data.Reviewed,
-                    Approved: req.data.Approved,
-                })
-            )
-            .then((resolve, reject) => {
-                console.log("Resolve", resolve);
-                console.log("Reject", reject);
-
-                if (typeof resolve !== "undefined") {
-                    return req.data;
-                } else {
-                    req.error(409, "Record Not Inserted");
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                req.error(err.code, err.message);
-            });
-        console.log("Before End", returnData);
-        return returnData;
-    });
-
-    srv.before("CREATE", "CreateOrder", (req) => {
-        req.data.CreatedOn = new Date().toISOString().slice(0, 10);
-        return req;
+      })
+      .catch((err) => {
+        console.log(err);
+        req.error(err.code, err.message);
       });
+    console.log("Before End", returnData);
+    return returnData;
+  });
+
+  srv.before("CREATE", "Orders", (req) => {
+    req.data.CreatedOn = new Date().toISOString().slice(0, 10);
+    return req;
+  });
 
   //************UPDATE******/
-  srv.on("UPDATE", "UpdateOrder", async (req) => {
+  srv.on("UPDATE", "Orders", async (req) => {
     let returnData = await cds
       .transaction(req)
       .run([
@@ -78,5 +78,30 @@ module.exports = (srv) => {
     console.log("Before End", returnData);
     return returnData;
   });
-    
+
+  //************DELETE******/
+  srv.on("DELETE", "Orders", async (req) => {
+    let returnData = await cds
+      .transaction(req)
+      .run(
+        DELETE.from(Orders).where({
+          ClientEmail: req.data.ClientEmail,
+        })
+      )
+      .then((resolve, reject) => {
+        console.log("Resolve", resolve);
+        console.log("Reject", reject);
+
+        if (resolve !== 1) {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        req.error(err.code, err.message);
+      });
+    console.log("Before End", returnData);
+    return await returnData;
+  });
+
 };
