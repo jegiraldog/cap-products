abc4b94a5443ff665927b5f1963b9daa4e1ef8d3
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2023-11-13 20:24:26 +0000

    2.3. CAP Server Bootstrap

---
+++ srv/server.js
@@ -0,0 +1,8 @@
+const cds = require("@sap/cds");
+cds.on("bootstrap", (app) =>{
+    app.get("/alive", (_, res) =>{
+        res.status(200).send("Server is Alive");
+    });
+});
+
+module.exports = cds.server;
