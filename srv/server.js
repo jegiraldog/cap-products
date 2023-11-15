64e5e7dc8938ea63649db0220f8a9ed322367c8d
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2023-11-15 23:45:33 +0000

    2.7. OData Adapter Proxy

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,9 @@
 const cds = require("@sap/cds");
-const cors = require('cors')
+const cors = require('cors');
+const adapterProxy = require("@sap/cds-odata-v2-adapter-proxy");
 
 cds.on("bootstrap", (app) =>{
+    app.use(adapterProxy());
     app.use(cors());
     app.get("/alive", (_, res) =>{
         res.status(200).send("Server is Alive");
