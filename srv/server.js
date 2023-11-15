db332bbe93c91f7790e48d37d3f75b60625c5d19
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2023-11-15 19:52:36 +0000

    2.4. CORS

--- srv/server.js
+++ srv/server.js
@@ -1,5 +1,8 @@
 const cds = require("@sap/cds");
+const cors = require('cors')
+
 cds.on("bootstrap", (app) =>{
+    app.use(cors());
     app.get("/alive", (_, res) =>{
         res.status(200).send("Server is Alive");
     });
